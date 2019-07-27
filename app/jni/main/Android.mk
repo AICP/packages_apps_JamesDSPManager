2a0838cd5cac6a28c64384e75f88df6ea83ad74c
Julian Veit <claymore1298@gmail.com>
2019-07-27 11:18:45 +0200

    JamesDSP: CFLAGS: build with -Wno-unused-variable -Wno-unused-parameter

--- app/jni/main/Android.mk
+++ app/jni/main/Android.mk
@@ -4,7 +4,7 @@ LOCAL_MODULE := jamesDSPImpulseToolbox
 LOCAL_SRC_FILES := kissfft/kiss_fft.c kissfft/kiss_fftr.c JdspImpResToolbox.c AutoConvolver.c
 LOCAL_STATIC_LIBRARIES = libsndfile libsamplerate
 LOCAL_CPPFLAGS += -ffunction-sections -fdata-sections -Ofast -ftree-vectorize -DNDEBUG
-LOCAL_CFLAGS += -ffunction-sections -fdata-sections -Ofast -ftree-vectorize -DNDEBUG
+LOCAL_CFLAGS += -ffunction-sections -fdata-sections -Ofast -ftree-vectorize -DNDEBUG -Wno-unused-variable -Wno-unused-parameter
 LOCAL_LDFLAGS += -Wl,--gc-sections,--exclude-libs,ALL
 include $(BUILD_SHARED_LIBRARY)
 
